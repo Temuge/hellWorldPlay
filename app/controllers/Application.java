bf18cab4cd4cf7745b2c23119fe6ca9d2fb185a6
Temuge <temuge@temuges-mbp.cable.rcn.com>
2014-12-29 22:31:08 -0500

    Bubu made boxes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,8 +8,10 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
+	private static String url1 = "http://www.sweetandsavorybyshinee.com/wp-content/uploads/2014/12/Orange-Hot-Chocolate-7-650x280.jpg";
 
     public static void index() {
+    	
         render();
     }
 }
